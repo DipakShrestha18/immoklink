16e790c38f4b49406bdad752064eafb82fed2d7e
shresthd <d.shrestha@wellcome.ac.uk>
2016-02-24 22:25:59 +0000

    css move to plugin.

--- www/sites/all/themes/immoklink_base/templates/system/html.tpl.php
+++ www/sites/all/themes/immoklink_base/templates/system/html.tpl.php
@@ -47,7 +47,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable = yes">
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <link rel="stylesheet" type="text/css" href="/sites/all/themes/immoklink_base/css/gallery/screen.css">
   <link rel="stylesheet" type="text/css" href="/sites/all/themes/immoklink_base/css/screen_styles.css" />
   <link rel="stylesheet" type="text/css" href="/sites/all/themes/immoklink_base/css/screen_layout_large.css" />
   <link rel="stylesheet" type="text/css" media="only screen and (min-width:50px) and (max-width:500px)" href="/sites/all/themes/immoklink_base/css/screen_layout_small.css" />
